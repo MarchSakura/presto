8e0c700fe13cdbd6c0308e91e895562df6fab344
hao <qishiqihao@gmail.com>
2021-04-22 20:25:56 +0800

    add tsing mirror

--- install_pkg.sh
+++ install_pkg.sh
@@ -5,5 +5,5 @@ yum install -y libX11 libX11-devel
 yum install -y xorg-x11-drivers.x86_64
 yum install -y python-devel
 yum install -y tkinter
-pip install numpy==1.16.0
-pip install Matplotlib==2.2.5
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple numpy==1.16.0
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Matplotlib==2.2.5
